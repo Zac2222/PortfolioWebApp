5cb7f286b0e6a91fbe5362c2b252a900a2ae6afb
zac2222 <zacrice2222@gmail.com>
2023-09-21 21:01:20 -0700

    added contact and project pages, aswell as color

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Contact = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <iframe
+      title="contact-form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLScZJ66E3AgGnMH977uRqNyIVHn2FwTqY7aAdYEN0CqKbCnspA/viewform?embedded=true"
+        style={{ width: "100%", height: "100vh", marginBottom: '-60px' }}
+      >
+        Loading…
+      </iframe>
 
-export default Contact
+      <Form className='d-flex  flex-column align-items-center mb-5'>
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter your name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formMessage">
+        <Form.Label>Message</Form.Label>
+        <Form.Control type="text" placeholder="Enter Message" />
+      </Form.Group>
+  
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+      <h2 className='boop emailText '>Or email me at: zacrice2222@gmail.com</h2>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const Projects = () => {
+  //not sure yet how we are integrating our projects, so they are just text for the time being
   return (
-    <div>
-      
+    <div className='d-flex flex-column align-items-center'>
+      <h2 className='boop d-flex justify-content-center'>Deal or no deal</h2>
+      <h3 className='boop noteText d-flex justify-content-center mb-5'>Unity, C#/2022</h3>
+
+      <h2 className='boop d-flex justify-content-center mt-5'>Guilty Gear Trivia</h2>
+      <h3 className='boop noteText d-flex justify-content-center mb-5'>JS, HTML, CSS/2023</h3>
+
+      <h2 className='boop d-flex justify-content-center mt-5'>Simple Calculator</h2>
+      <h3 className='boop noteText d-flex justify-content-center mb-5'>JS, HTML, CSS/2023</h3>
+
+      <h2 className='boop d-flex justify-content-center mt-5'>To Do List</h2>
+      <h3 className='boop noteText d-flex justify-content-center mb-5'>JS, HTML, CSS/2023</h3>
+
+      <h2 className='boop d-flex justify-content-center mt-5'>Tic Tac Toe</h2>
+      <h3 className='boop noteText d-flex justify-content-center mb-5'>React, TypeScript/2023</h3>
+
+      <h2 className='boop d-flex justify-content-center mt-5'>Weather App</h2>
+      <h3 className='boop noteText d-flex justify-content-center mb-5'>JS, HTML, CSS/2023</h3>
     </div>
   )
 }
