77a3b77214898cf3ccdd35058b5529eed5621663
Zac Rice <csa_zrice@sjcoe.net>
2024-03-25 10:45:20 -0700

    added page transition effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,69 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import './CSS/App.css'
 import AboutMe from './components/AboutMe';
 import Background from './components/Background'
 import Landing from './components/Landing';
 import Menu from './components/Menu'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    x: "100vw",
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+  out: {
+    opacity: 0,
+    x: "-100vw",
+  }
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "easeInOut",
+  duration: 0.4,
+};
+
+const Page = ({ children }: { children: React.ReactNode }) => (
+  <motion.div
+    initial="initial"
+    animate="in"
+    exit="out"
+    variants={pageVariants}
+    transition={pageTransition}
+  >
+    {children}
+  </motion.div>
+);
+
+const AppRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.key}>
+        <Route path="/" element={<Page><Landing /></Page>} />
+        <Route path="/AboutMe" element={<Page><AboutMe /></Page>} />
+        <Route path="/Projects" element={<Page><Projects /></Page>} />
+        <Route path="/Contact" element={<Page><Contact /></Page>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Menu/>
       <Background/>
-      <Routes>
-        <Route path="/" element={<Landing/>} />
-        <Route path="/AboutMe" element={<AboutMe />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-      {/* <Background /> */}
-      
-    </BrowserRouter>
-  )
-}
+      <AppRoutes />
+    </Router>
+  );
+};
 
-export default App
+export default App;
 
